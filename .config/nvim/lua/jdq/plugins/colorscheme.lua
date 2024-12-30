8a8ca020b0e1223a967678110c935e64e3f10265
0x6A6471 <hello@jakequinter.io>
2024-12-30 17:38:38 -0600

    gruvbuddy

--- .config/nvim/lua/jdq/plugins/colorscheme.lua
+++ .config/nvim/lua/jdq/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-	{
-		"ramojus/mellifluous.nvim",
-		name = "mellifluous",
-		config = function()
-			require("mellifluous").setup({
-				transparent_background = {
-					enabled = true,
-				},
-			})
-			vim.cmd("colorscheme mellifluous")
-			vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-			vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
-		end,
-	},
+	-- {
+	-- 	"ramojus/mellifluous.nvim",
+	-- 	name = "mellifluous",
+	-- 	config = function()
+	-- 		require("mellifluous").setup({
+	-- 			transparent_background = {
+	-- 				enabled = true,
+	-- 			},
+	-- 		})
+	-- 		vim.cmd("colorscheme mellifluous")
+	-- 		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+	-- 		vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
+	-- 	end,
+	-- },
 
 	-- {
 	-- 	"catppuccin/nvim",
@@ -35,31 +35,32 @@ return {
 	-- 	end,
 	-- },
 
-	-- {
-	-- 	"tjdevries/colorbuddy.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		local colorbuddy = require("colorbuddy")
-	-- 		local Color = colorbuddy.Color
-	-- 		local colors = colorbuddy.colors
-	-- 		local Group = colorbuddy.Group
-	-- 		-- local groups = colorbuddy.groups
-	-- 		local styles = colorbuddy.styles
-	--
-	-- 		Color.new("qwhite", "#ffffff")
-	--
-	-- 		Group.new("htmlTag", colors.yellow:light())
-	-- 		Group.new("htmlTagName", colors.violet)
-	-- 		Group.new("typescriptbraces", colors.qwhite)
-	-- 		Group.new("typescriptEndColons", colors.qwhite)
-	-- 		Group.new("typescriptCall", colors.qwhite)
-	-- 		Group.new("typescriptStatementKeyword", colors.violet)
-	-- 		Group.new("WinSeparator", nil, nil)
-	--
-	-- 		vim.cmd.colorscheme("gruvbuddy")
-	-- 		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-	-- 		vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
-	-- 	end,
-	-- },
+	{
+		"tjdevries/colorbuddy.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local colorbuddy = require("colorbuddy")
+			local Color = colorbuddy.Color
+			local colors = colorbuddy.colors
+			local Group = colorbuddy.Group
+
+			Color.new("qwhite", "#ffffff")
+
+			Group.new("htmlTag", colors.yellow:light())
+			Group.new("htmlTagName", colors.violet)
+			Group.new("typescriptbraces", colors.qwhite)
+			Group.new("typescriptEndColons", colors.qwhite)
+			Group.new("typescriptCall", colors.qwhite)
+			Group.new("tsxAttrib", colors.yellow:light())
+			Group.new("typescriptStatementKeyword", colors.violet)
+			Group.new("@lsp.type.function.ocaml_mlx", colors.violet)
+			Group.new("WinSeparator", nil, nil)
+
+			vim.cmd.colorscheme("gruvbuddy")
+			vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+			vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#000000" })
+			vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
+		end,
+	},
 }
