238e7ef72279c111464be63e6ad0d0c2fac005f0
0x6A6471 <hello@jakequinter.io>
2025-02-05 10:48:43 -0600

    awesomewm

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -254,9 +254,11 @@ awful.screen.connect_for_each_screen(function(s)
 		s.mytasklist, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
-			mykeyboardlayout,
-			wibox.widget.systray(),
+			-- mykeyboardlayout,
+			-- wibox.widget.systray(),
+			wibox.widget.textbox(" "),
 			mytextclock,
+			wibox.widget.textbox(" "),
 			s.mylayoutbox,
 		},
 	})
