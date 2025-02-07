c46aafdf86cf345063a9398d466625ad605da4f8
0x6A6471 <hello@jakequinter.io>
2025-02-07 13:58:16 -0600

    awesome menu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -111,24 +111,24 @@ myawesomemenu = {
 	},
 }
 
--- local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
---
--- if has_fdo then
--- 	mymainmenu = freedesktop.menu.build({
--- 		before = { menu_awesome },
--- 		after = { menu_terminal },
--- 	})
--- else
--- 	mymainmenu = awful.menu({
--- 		items = {
--- 			menu_awesome,
--- 			{ "Debian", debian.menu.Debian_menu.Debian },
--- 			menu_terminal,
--- 		},
--- 	})
--- end
+local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
 
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
+if has_fdo then
+	mymainmenu = freedesktop.menu.build({
+		before = { menu_awesome },
+		after = { menu_terminal },
+	})
+else
+	mymainmenu = awful.menu({
+		items = {
+			menu_awesome,
+			{ "Debian", debian.menu.Debian_menu.Debian },
+			menu_terminal,
+		},
+	})
+end
+
+-- mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
