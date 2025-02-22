8c56fa2f60a0ff3d0ae540f8daf4d45eea9f5efe
0x6A6471 <hello@jakequinter.io>
2025-02-22 16:32:09 -0600

    cleanup

--- .config/nvim/lua/jdq/plugins/lsp/lsp.lua
+++ .config/nvim/lua/jdq/plugins/lsp/lsp.lua
@@ -7,7 +7,6 @@ return {
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
-		local configs = require("lspconfig.configs")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
@@ -47,26 +46,6 @@ return {
 		end
 
 		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		local lexical_config = {
-			filetypes = { "elixir", "eelixir", "heex" },
-			cmd = { vim.fn.expand("~/Developer/oss/lexical/_build/dev/package/lexical/bin/start_lexical.sh") },
-			settings = {},
-		}
-
-		if not configs.lexical then
-			configs.lexical = {
-				default_config = {
-					filetypes = lexical_config.filetypes,
-					cmd = lexical_config.cmd,
-					root_dir = function(fname)
-						return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-					end,
-					-- optional settings
-					settings = lexical_config.settings,
-				},
-			}
-		end
 
 		lspconfig.lexical.setup({})
 
