edf7f5f6d75690dbf73395ec9a01baf507e7e43f
0xA6471 <x6a6471@fastmail.com>
2026-06-21 12:40:49 -0500

    tmux bindings for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,6 +27,58 @@ end
 bind \cf fcd
 set -gx SHELL /usr/bin/fish
 
+# tmux
+function tsearch --description "Search scrollback across all tmux panes"
+    if not command -q tmux
+        echo "tsearch: tmux is not installed" >&2
+        return 1
+    end
+
+    if test (count $argv) -eq 0
+        echo "usage: tsearch <pattern>"
+        return 2
+    end
+
+    set -l pattern (string join " " -- $argv)
+
+    for pane in (tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index}')
+        tmux capture-pane -pt $pane -S - -p | rg -n --color=always -- "$pattern"
+        if test $pipestatus[2] -eq 0
+            set -l title (tmux display-message -pt $pane -p '#{session_name}:#{window_index}.#{pane_index} #{window_name} #{pane_current_command}')
+            printf '\n[%s]\n\n' "$title"
+        end
+    end
+end
+
+function tfzf --description "Fuzzy pick a tmux session"
+    if not command -q tmux
+        echo "tfzf: tmux is not installed" >&2
+        return 1
+    end
+
+    if not command -q fzf
+        echo "tfzf: fzf is not installed" >&2
+        return 1
+    end
+
+    set -l session (tmux list-sessions -F '#{session_name}: #{session_windows} windows (created #{session_created_string})' | fzf --ansi --no-sort | string split -m1 ':' | head -n1)
+
+    if test -z "$session"
+        commandline -f repaint
+        return
+    end
+
+    if set -q TMUX
+        tmux switch-client -t "$session"
+    else
+        tmux attach-session -t "$session"
+    end
+
+    commandline -f repaint
+end
+
+bind \ct tfzf
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
